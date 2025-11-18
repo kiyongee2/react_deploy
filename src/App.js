38d79205f35cc480d8ee42be6e2c1a802ae0ee1b
kiyongee2 <kiyongee2@gmail.com>
2025-11-18 11:07:56 +0900

    App.js update

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import InputText from './components/InputText';
 import UseEffect from './components/UseEffect';
 
 function App() {
-  // const msg="이것은 샘플 메시지입니다.";
   
   return (
     <div className="App">
-      <Counter color="red" title="Hello, React" message="이것은 속성의 값입니다." />
+      <Counter color="red" title="Hello, React" />
       <InputText color="green" title="입력 상자 예제" />
       <ArrayMap color="blue" title="데이터 목록" />
       <UseEffect title="useEffect 훅 사용" />
